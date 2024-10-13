45cda4c86458fd38930b10907a67f4fb809645df
Tayyeb Shahzad <tayyebbutt966@gmail.com>
2024-10-13 11:57:54 +0500

    frontend updating

--- backend/models/user.js
+++ backend/models/user.js
@@ -23,15 +23,6 @@ const userSchema = new mongoose.Schema({
     }
 }, { timestamps: true }); // Automatically adds createdAt and updatedAt fields
 
-// using bryptjs to hash passwords
-userSchema.pre('save', async function(next) {
-    if (!this.isModified('password')) {
-        return next();
-    }
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
-});
 
 // Create an index for the email field to ensure uniqueness
 userSchema.index({ email: 1 }, { unique: true });
--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -13,20 +13,53 @@ router.post('/register', async (req, res) => {
     let email = req.body.email
     let password = req.body.password
     let role = req.body.role
-    
-    const user = new User({
-        name: name,
-        email: email,
-        password: password,
-        role: role
+
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const record = await User.findOne({
+        email: email
     })
 
-    const result = await user.save();
+// validating registered user
 
+    if (record)
+    {
+        res.status(400).send({
+            message: "Email is already Registered !"
+        })
+    }
+else
+    {
+        const user = new User({
+            name: name,
+            email: email,
+            password: hashedPassword,
+            role: role
+        })
+        const result = await user.save();
+
+        // JWT Token
+        const { _id } = await result.toJSON()
+        
+        const token = jwt.sign({ _id: _id }, "secret");
+        
+        res.cookie( "jwt", token,{
+            httpOnly: true,
+            maxAge: 24 * 60 * 60 * 1000   // 1 day  
+        })
+        
+        res.send({
+          message: "success"
+      })
+
+        // giving back respond to post request
     res.json({
         user: result
     })
 
+        }
 });
 
 router.post('/login', async (req, res) => {
